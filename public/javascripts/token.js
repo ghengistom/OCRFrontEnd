5a4d18d1bc35c7f723d46bcb78a652cd2d48e4fd
Your Name <tom.bernard@activepdf.com>
2017-07-18 16:06:14 -0700

    Added token.html and token.js to retrieve token from OCR server

---
+++ public/javascripts/token.js
@@ -0,0 +1,10 @@
+console.log("In CLient token.js");
+
+var x = document.body;
+
+document.write(Date());
+document.getElementById("token").innerHTML = x;
+//document.getElementById('token').innerHTML = x ;
+
+
+//console.log("");
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
+
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+
+
 });
+
+//Get token
+router.get('/token', function(req, res, next){
+  //go to url and get the token.
+  res.render('token', {title:'OCR Token'});
+  request({
+    uri: 'http://localhost:52525/api/Account/token'
+  },
+  function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      console.log(body);
+
+    }
+  }
+)
+});
+
+
+//Get Token
+/*
+client.get("http://localhost:52525/api/Account/token", function (data, response){
+  //parsed response body as js object
+  console.log(data);
+  //raw response
+  console.log(response);
+});
+*/
+// registering remote methods
+
 
 module.exports = router;
